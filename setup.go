6bd4455d73d9ba83c0c965cfff72894a8f5b1437
&#10059 <90846584+theokbokki@users.noreply.github.com>
2023-08-18 08:38:12 +0200

    🔨 Add all files to be moved for nuxt
    References #1

--- setup.go
+++ setup.go
@@ -19,7 +19,7 @@ func (m model) Setup(f Framework) {
 	m.bibi.Text = "Cloning the bibimbap repo"
 
 	Clone()
-	MoveFiles(f)
+	m.MoveFiles(f)
 }
 
 func Clone() {
@@ -37,13 +37,29 @@ func Clone() {
 	CheckIfError(err)
 }
 
-func MoveFiles(f Framework) {
-	err := os.Rename("./tmp/shared/.gitignore", "./my-project/.gitignore")
+func (m model) MoveFiles(f Framework) {
+	m.bibi.Text = "Moving files in the right place"
+
+	err := os.Mkdir("./my-project", 0755)
+	err = os.Rename("./tmp/shared/.gitignore", "./my-project/.gitignore")
 	err = os.Rename("./tmp/shared/.npmrc", "./my-project/.npmrc")
 	err = os.Rename("./tmp/shared/apps", "./my-project/apps")
 	err = os.Rename("./tmp/shared/package.json", "./my-project/package.json")
 	err = os.Rename("./tmp/shared/packages", "./my-project/packages")
 	err = os.Rename("./tmp/shared/pnpm-workspace.yaml", "./my-project/pnpm-workspace.yaml")
+
+	if f.Title == "Nuxt" {
+		err = os.Rename("./tmp/nuxt/pnpm-lock.yaml", "./my-project/pnpm-lock.yaml")
+		err = os.Rename("./tmp/nuxt/desktop", "./my-project/apps/desktop")
+		err = os.Rename("./tmp/shared/src-tauri", "./my-project/apps/desktop/src-tauri")
+		err = os.Rename("./tmp/nuxt/tauri.conf.json", "./my-project/apps/desktop/src-tauri/tauri.conf.json")
+		err = os.Rename("./tmp/nuxt/ui/components", "./my-project/packages/ui/lib/components")
+		err = os.Rename("./tmp/nuxt/ui/index.ts", "./my-project/packages/ui/index.ts")
+		err = os.Rename("./tmp/nuxt/ui/package.json", "./my-project/packages/ui/package.json")
+		err = os.Rename("./tmp/nuxt/web/package.json", "./my-project/apps/web/package.json")
+		err = os.Rename("./tmp/nuxt/web/js", "./my-project/apps/web/resources/js")
+		err = os.Rename("./tmp/nuxt/web/vite.config.js", "./my-project/apps/web/vite.config.js")
+	}
 
 	CheckIfError(err)
 }
